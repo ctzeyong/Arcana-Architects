17abd8735eb9da5990aded63227bca2d31f57283
ljunfun <junfunlim@gmail.com>
2024-07-25 16:52:45 +0800

    transition scene buttons
    somehow github didnt upload properly last night hence reupload

--- LevelFailed.gd
+++ LevelFailed.gd
@@ -1,10 +1,7 @@
 extends CanvasLayer
 
-
-
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
 
-
 func _on_menu_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Menu.gd")
