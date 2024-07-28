c310c4de1eefe3b82d65bca13fabbd216a70f91b
ljunfun <junfunlim@gmail.com>
2024-07-28 22:34:26 +0800

    quick bug fixes

--- artifacts/artifact2.gd
+++ artifacts/artifact2.gd
@@ -6,5 +6,6 @@ func _on_next_level_button_pressed():
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
 
-func _on_quit_button_pressed():
-	get_tree().quit()
+
+func _on_quit_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
