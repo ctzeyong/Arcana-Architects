bb55fccdded6649751a6e10578edb819c4a7481c
ljunfun <junfunlim@gmail.com>
2024-07-28 15:52:35 +0800

    artifact 2 screen and polish up of level 2

--- Levels/LevelPassed2.gd
+++ Levels/LevelPassed2.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 func _on_continue_button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://artifacts/artifact1.tscn")
+	get_tree().change_scene_to_file("res://artifacts/artifact2.tscn")
 
 
 func _on_restart_button_pressed():
---
+++ artifacts/artifact2.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_next_level_button_pressed():
+	%ToBeContinued.visible = true
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
