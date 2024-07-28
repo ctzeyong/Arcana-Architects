cb897b4aa6731796ad389562e3cb2c3c63d505a1
ljunfun <junfunlim@gmail.com>
2024-07-28 21:01:58 +0800

    quick addtion to options in main menu

--- Menu.gd
+++ Menu.gd
@@ -4,8 +4,11 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_options_button_pressed():
-	pass # Replace with function body.
+	%Help.visible = true
 
 func _on_quit_button_pressed():
-		get_tree().quit()
-	
+	get_tree().quit()
+
+
+func _on_return_button_pressed():
+	%Help.visible = false
